ed10682a35160533fb843da32d0365573cdd3519
Jack Sutton <jack.sutton2013@my.ntu.ac.uk>
2021-05-12 10:02:23 +0100

    Import Shapefiles_LTLA

---
+++ importing_shapefiles_LTLA.R
@@ -0,0 +1,71 @@
+library(sf)
+library(raster)
+library(dplyr)
+library(spData)
+
+
+library(tmap)    # for static and interactive maps
+library(ggplot2) # tidyverse data visualization package
+library(rgdal)
+library("png")
+library(rgeos)
+library(tmap)
+#match shapefiles so that they are identical to LTLA regions defined in datasets
+
+
+#download shape files, store in directory and import here.
+#bournemouth, christchurch and poole
+shp2 <- shapefile("C:/Users/Owner/Desktop/COVID/COVID_update/geomap/Bournemouth, Christchurch, Poole/Counties_and_Unitary_Authorities_December_2019_Boundaries_UK_BFC.shp")
+#wales
+shp3 <- shapefile("C:/Users/Owner/Desktop/COVID/COVID_update/geomap/wales/wales_lad_2011.shp")
+#cornwall and isles of scilly
+shp4 <- shapefile("C:/Users/Owner/Desktop/COVID/COVID_update/geomap/Cornwall and Isles of Scilly/england_healthauth_2001.shp")
+#england with removed regions
+shp5 <- shapefile("C:/Users/Owner/Desktop/COVID/COVID_update/geomap/england_remove_regions/england_lad_2011.shp")
+
+
+#the next chuck of code is some simple manipulation so that names of regions and regional cases are alighned to PHE
+#select all code and run.
+#bind shape files together
+shp <- bind(shp2, shp3, shp4)
+shp<-shp[-c(25:41),]
+shp <- bind(shp, shp5)
+#remove unwanted columns
+drops <- c("objectid", "ctyua19cd", "ctyua19nmw", "bng_e", "bng_n", "long", "lat", "st_areasha", "st_lengths", "label", "altname", "code")
+shp<-shp[ , !(names(shp) %in% drops)]
+shp$name[1]<- "Bournemouth, Christchurch and Poole"
+drops <- c("ctyua19nm")
+shp<-shp[ , !(names(shp) %in% drops)]
+names(shp@data) <- "region"
+#england with removed regions
+shp6 <- shapefile("C:/Users/Owner/Desktop/COVID/COVID_update/geomap/england with combined regions/Local_Authority_Districts__April_2019__UK_BFE_v2.shp")
+shp6 <- shp6[order(shp6$LAD19NM),]
+shp6<-shp6[-c(1:97),]
+shp6<-shp6[-c(2:18),]
+shp6<-shp6[-c(3:17),]
+shp6<-shp6[-c(4:157),]
+shp6<-shp6[-c(5:81),]
+shp6<-shp6[-c(6:22),]
+#remove unwanted columns
+drops <- c("OBJECTID", "LAD19CD", "LAD19NMW", "BNG_E", "BNG_N", "LONG", "LAT", "Shape__Are", "Shape__Len")
+shp6<-shp6[ , !(names(shp6) %in% drops)]
+names(shp6@data) <- "region"
+shp <- bind(shp, shp6)
+shp <- shp[order(shp$region),]
+shp$region[291]<- "Vale of Glamorgan"
+shp$region[185]<- "Newcastle under Lyme"
+shp$region[186]<- "Newcastle upon Tyne"
+shp<-shp[-319,]
+#combined
+shp7 <- shapefile("C:/Users/Owner/Desktop/COVID/COVID_update/geomap/combined/combined.shp")
+shp7 <- shp7[order(shp7$name),]
+shp7<-shp7[-c(1:61),]
+shp7<-shp7[-c(2:105),]
+names(shp7@data) <- "region"
+shp7<-shp7[,-c(2:4)]
+shp7<-shp7[,-c(2:10)]
+shp <- bind(shp, shp7)
+shp <- shp[order(shp$region),]
+#shp<-shp[-88,]
+shp$region[88]<- "Hackney and City of London"
+shp <- shp[order(shp$region),]
